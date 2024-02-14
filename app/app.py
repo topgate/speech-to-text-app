32a793806709294fb0c5129b7d4eb27db360e0d7
r-sakamoto <99632267+r-sakamoto-tg@users.noreply.github.com>
2024-02-14 15:24:56 +0900

    speech-to-text app

--- app/app.py
+++ app/app.py
@@ -1,16 +1,40 @@
-from http.server import HTTPServer
-from http.server import BaseHTTPRequestHandler
+import gradio as gr
+# Import the Speech-to-Text client library
+from google.cloud import speech
+
+# Instantiates a client
+client = speech.SpeechClient()
 
 
-class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
+def transcribe_speech(file):
+    with open(file.name, 'rb') as f:
+        content = f.read()
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write('Hello, World!'.encode())
+    audio = speech.RecognitionAudio(content=content)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=44100,
+        language_code="en-US",
+        model="phone_call",
+        audio_channel_count=2,
+        enable_word_confidence=True,
+        enable_word_time_offsets=True,
+    )
+
+    # Detects speech in the audio file
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    response = operation.result(timeout=90)
+
+    ret = ""
+    for result in response.results:
+        ret += result.alternatives[0].transcript
+
+    return ret
 
 
-server_address = ('0.0.0.0', 8080)
-httpd = HTTPServer(server_address, CustomHTTPRequestHandler)
-httpd.serve_forever()
+demo = gr.Interface(fn=transcribe_speech, inputs="file",
+                    outputs="text", allow_flagging='never')
+
+demo.launch(server_name="0.0.0.0", server_port=8080)
